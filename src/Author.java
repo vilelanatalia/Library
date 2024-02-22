42af97216a017a089c28f95680037c854de622ad
vilelanatalia <nataliavilela79@gmail.com>
2024-02-22 13:58:53 -0300

    add Author class

---
+++ src/Author.java
@@ -0,0 +1,12 @@
+public class Author {
+    private String name;
+
+    // Constructor
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
